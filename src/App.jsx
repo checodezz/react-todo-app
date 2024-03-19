a682a73aed4c92073afa5e288578d413424cc908
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-19 04:39:52 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+    <main>
+      React ⚛️ + Vite ⚡ + Replit
+    </main>
+      </>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="">
+      <div>
+        <h1>Todos</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
