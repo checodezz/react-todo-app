aae6bbb799f9cc2637c8fa859efbc13e672d1bbb
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-19 05:50:22 +0000

    added footer and main image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import Header from './components/Header'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Link } from "react-router-dom"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
-      <Header/>
-    <main className="container">
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
-      </>
-  )
+      <Header />
+      <main className="container ">
+        <div className="container my-4 text-center">
+          <img src="https://cdn.pixabay.com/photo/2019/09/17/09/45/to-do-4483048_1280.jpg" className="img-fluid" width="700" height="400" alt="to do list image"/>
+          <h2 className="my-3">Todos</h2>
+          <p>Welcome to the Todo application!</p>
+<Link className="btn btn-primary">View Todos</Link>
+        </div>
+      
+      </main>
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="py-2 bg-dark text-light">
+    <p>&copy; {new Date().getFullYear()} | Todo App</p>
+    </footer>
+  )
+}
+export default Footer
