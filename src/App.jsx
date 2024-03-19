e6c2108478d45af94e389ed8d80c162622e4ea9a
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-19 05:01:01 +0000

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 import Header from './components/Header'
 
 export default function App() {
   return (
     <>
       <Header/>
-    <main>
+    <main className="container">
       React ⚛️ + Vite ⚡ + Replit
     </main>
       </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+import {NavLink} from "react-router-dom"
+
 const Header = () => {
   return (
-    <header className="">
-      <div>
-        <h1>Todos</h1>
+    <header className=" bg-dark text-light">
+      <div className="container py-4">
+        <h1 className="display-1">Todos</h1>
         <nav>
-          <ul>
-            <li>Home</li>
+          <ul className="nav">
+            <li className="nav-item"><NavLink className="nav-link">Home</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link">Todo</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link">About</NavLink></li>
           </ul>
         </nav>
       </div>
